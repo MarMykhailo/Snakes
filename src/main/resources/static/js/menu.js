a75f890ddc2c7e39a6aca92a135df0afbd0ee27d
MarMykhailo <mykhailo.marochkanych.pz.2022@lpnu.ua>
2024-11-22 21:34:21 +0200

    Працює видалення

--- src/main/resources/static/js/menu.js
+++ src/main/resources/static/js/menu.js
@@ -9,10 +9,10 @@ function createRoom() {
         body: JSON.stringify({ name: roomName, type: roomType })
     }).then(response => {
         if (response.ok) {
-            alert("Room created successfully");
+            //alert("Room created successfully");
             updateRooms();
         } else {
-            alert("Error creating room");
+            //alert("Error creating room");
         }
     }).catch(error => {
         console.error('Error:', error);
@@ -56,12 +56,58 @@ function addRoomToTable(room) {
 
     cell1.innerHTML = room.type;
     cell2.innerHTML = room.name;
-    cell3.innerHTML = '<button onclick="joinRoom(\'' + room.name + '\')">Долучитися</button>';
+    cell3.innerHTML =
+    '<button onclick="joinRoom(\'' + room.id + '\')">' +
+    '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-door-closed" viewBox="0 0 16 16">' +
+        '<path d="M3 2a1 1 0 0 1 1-1h8a1 1 0 0 1 1 1v13h1.5a.5.5 0 0 1 0 1h-13a.5.5 0 0 1 0-1H3zm1 13h8V2H4z"/>' +
+        '<path d="M9 9a1 1 0 1 0 2 0 1 1 0 0 0-2 0"/>' +
+    '</svg>' +
+    '</button>' +
+    '<button onclick="deleteRoom(\'' + room.id + '\')">' +
+        '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">' +
+            '<path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/>' +
+        '</svg>' +
+    '</button>';
 }
 
-function joinRoom(roomName) {
+function joinRoom(id) {
     // Implement the logic to join the room
-    alert("Joining room: " + roomName);
+    alert("Joining room: " + id);
+    fetch('/join-room', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ id: id })
+    }).then(response => {
+        if (response.ok) {
+            window.location.href = "/game";
+        } else {
+            alert("Error joining room");
+        }
+    }).catch(error => {
+        console.error('Error:', error);
+        alert("Error joining room");
+    });
+}
+
+function deleteRoom(id) {
+    fetch(`/delete-room/${id}`, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(response => {
+        if (response.ok) {
+            alert("Room deleted successfully");
+            updateRooms();
+        } else {
+            alert("Error deleting room");
+        }
+    }).catch(error => {
+        console.error('Error:', error);
+        alert("Error deleting room");
+    });
 }
 
 
